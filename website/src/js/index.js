f67d24f155426481172c7055fe87ea39b146a512
DLFDK <dennislythfrederiksen@gmail.com>
2021-11-30 06:16:40 +0100

    Write optimized draw function

--- website/src/js/index.js
+++ website/src/js/index.js
@@ -12,45 +12,45 @@ async function main() {
         indoorT: 22,
         solarArea: 20,
         efficiency: .6,
-        htc: 100,
+        htc: 250,
         storage: 50
     };
     const data = await fetch("js/data.json").then((response => response.json()));
+    const pointSize = 2;
     canvas.width = container.offsetWidth;
     canvas.height = container.offsetHeight;
-    const baseline = canvas.height - 5;
-    const scaleFactorWidth = (canvas.width - 4) / 365;
-    ctx.strokeStyle = "#4CE0D2";
+    const baseline = canvas.height - pointSize;
+    const scaleFactorWidth = (canvas.width - pointSize) / 365;
     ctx.fillStyle = "#4CE0D2";
-    drawPoint();
+    draw();
     const range_solar = document.getElementById("solar");
     const value_solar = document.getElementById("solarValue");
     range_solar.addEventListener("input", (event => {
         const value = event.target.value;
         value_solar.textContent = `${value < 10 ? "" : ""}${value}`;
         parameters["solarArea"] = event.target.value;
-        drawPoint();
+        draw();
     }));
     const range_efficiency = document.getElementById("efficiency");
     const value_efficiency = document.getElementById("efficiencyValue");
     range_efficiency.addEventListener("input", (event => {
         value_efficiency.textContent = event.target.value;
         parameters["efficiency"] = event.target.value / 100;
-        drawPoint();
+        draw();
     }));
     const range_htc = document.getElementById("htc");
     const value_htc = document.getElementById("htcValue");
     range_htc.addEventListener("input", (event => {
         value_htc.textContent = event.target.value;
         parameters["htc"] = event.target.value;
-        drawPoint();
+        draw();
     }));
     const range_storage = document.getElementById("storage");
     const value_storage = document.getElementById("storageValue");
     range_storage.addEventListener("input", (event => {
         value_storage.textContent = event.target.value;
         parameters["storage"] = event.target.value;
-        drawPoint();
+        draw();
     }));
     function drawPoint() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -79,36 +79,61 @@ async function main() {
                 result = 0;
             }
             state.push(result);
-            ctx.fillRect((i + 1) % 365 * scaleFactorWidth, baseline - state[i + 1] / 1e3 * scaleFactorHeight, 2, 2);
+            const x = i % 365 * scaleFactorWidth;
+            const y = baseline - state[i + 1] / 1e3 * scaleFactorHeight;
+            ctx.fillRect(x, y, pointSize, pointSize);
         }
         value_surplus.textContent = Math.round(surplus / 11 / 1e3, 0);
         value_deficit.textContent = Math.round(deficit / 11 / -1e3, 0);
         value_full.textContent = Math.round(full / 11, 0);
         value_empty.textContent = Math.round(empty / 11, 0);
     }
-    function drawPath() {
+    function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.beginPath();
         const state = [];
-        state.push(parameters["storage"] * 1e3);
+        const limit = parameters["storage"] * 1e3;
+        state.push(limit);
         const scaleFactorHeight = baseline / parameters["storage"];
-        ctx.moveTo(0, baseline - parameters["storage"] * scaleFactorHeight);
-        let gain;
-        let loss;
-        let result;
+        let deficit = 0;
+        let empty = 0;
+        let surplus = 0;
+        let full = 0;
+        let position = 0;
         for (let i = 0; i < data["Energy"].length; i++) {
-            gain = parameters["solarArea"] * data["Energy"][i];
-            loss = Math.min(parameters["htc"] * (data["Temperature"][i] - parameters["indoorT"]) * 24, 0);
-            result = Math.min(state[i] + gain + loss, parameters["storage"] * 1e3);
-            result = Math.max(result, 0);
-            state.push(result);
-            if ((i + 1) % 365 === 0) {
-                ctx.moveTo(0, baseline - state[i + 1] / 1e3 * scaleFactorHeight);
+            const gain = parameters["solarArea"] * data["Energy"][i] * parameters["efficiency"];
+            const loss = Math.min(parameters["htc"] * (data["Temperature"][i] - parameters["indoorT"]) * 24, 0);
+            const result = gain + loss + state[i];
+            if (result > limit) {
+                state.push(limit);
+                full++;
+                surplus = surplus + result - limit;
+            } else if (result < 0) {
+                state.push(0);
+                empty++;
+                deficit = deficit + result;
             } else {
-                ctx.lineTo((i + 1) % 365, baseline - state[i + 1] / 1e3 * scaleFactorHeight);
+                state.push(result);
+            }
+            if (i == 0) continue;
+            if (state[i + 1] !== state[i]) {
+                index = i % 365;
+                if (position <= index) {
+                    const x = position * scaleFactorWidth;
+                    const y = baseline - state[i] / 1e3 * scaleFactorHeight;
+                    ctx.fillRect(x, y, index - position + pointSize, pointSize);
+                } else {
+                    const x = position * scaleFactorWidth;
+                    const y = baseline - state[i] / 1e3 * scaleFactorHeight;
+                    ctx.fillRect(x, y, 365 - position, pointSize);
+                    ctx.fillRect(0, y, index - 0, pointSize);
+                }
+                position = index;
             }
         }
-        ctx.stroke();
+        value_surplus.textContent = Math.round(surplus / 11 / 1e3, 0);
+        value_deficit.textContent = Math.round(deficit / 11 / -1e3, 0);
+        value_full.textContent = Math.round(full / 11, 0);
+        value_empty.textContent = Math.round(empty / 11, 0);
     }
 }
 //# sourceMappingURL=index.js.map
--- website/src/js/index/main.js
+++ website/src/js/index/main.js
@@ -13,54 +13,49 @@ async function main() {
         indoorT: 22, // degC
         solarArea: 20, // m2
         efficiency: 0.6, // Fraction
-        htc: 100, // W/degC
+        htc: 250, // W/degC
         storage: 50 // kWh
     }
 
     const data = await fetch("js/data.json").then(response => response.json());
 
-    // canvas.width = data["Energy"].length + 1;
+    const pointSize = 2;
     canvas.width = container.offsetWidth;
     canvas.height = container.offsetHeight;
-    const baseline = canvas.height - 5;
-    // const scaleFactorHeight = baseline / parameters["storage"];
+    const baseline = canvas.height - pointSize;
+    const scaleFactorWidth = (canvas.width - pointSize) / 365;
 
-    const scaleFactorWidth = (canvas.width - 4 ) / 365;
-    ctx.strokeStyle = '#4CE0D2';
     ctx.fillStyle = '#4CE0D2';
-
-    drawPoint();
+    draw();
 
     const range_solar = document.getElementById("solar");
     const value_solar = document.getElementById("solarValue");
     range_solar.addEventListener("input", (event) => {
         const value = event.target.value;
-        // value_solar.textContent = `${value}${value / Math.round(value, 0) == 1 ? ".0" : ""} `;
         value_solar.textContent = `${value < 10 ? "" : ""}${value}`;
-        // value_solar.textContent = event.target.value;
         parameters["solarArea"] = event.target.value;
-        drawPoint();
+        draw();
     });
     const range_efficiency = document.getElementById("efficiency");
     const value_efficiency = document.getElementById("efficiencyValue");
     range_efficiency.addEventListener("input", (event) => {
         value_efficiency.textContent = event.target.value;
         parameters["efficiency"] = event.target.value / 100;
-        drawPoint();
+        draw();
     });
     const range_htc = document.getElementById("htc");
     const value_htc = document.getElementById("htcValue");
     range_htc.addEventListener("input", (event) => {
         value_htc.textContent = event.target.value;
         parameters["htc"] = event.target.value;
-        drawPoint();
+        draw();
     });
     const range_storage = document.getElementById("storage");
     const value_storage = document.getElementById("storageValue");
     range_storage.addEventListener("input", (event) => {
         value_storage.textContent = event.target.value;
         parameters["storage"] = event.target.value;
-        drawPoint();
+        draw();
     });
 
     function drawPoint() {
@@ -80,81 +75,85 @@ async function main() {
         let surplus = 0;
         let full = 0;
 
-        // console.time("Draw")
         for (let i = 0; i < data["Energy"].length; i++) {
             gain = parameters["solarArea"] * data["Energy"][i] * parameters["efficiency"];
             loss = Math.min(parameters["htc"] * (data["Temperature"][i] - parameters["indoorT"]) * 24, 0);
 
             result = gain + loss + state[i];
 
-            if(result > limit){
+            if (result > limit) {
                 full++;
                 surplus = surplus + result - limit;
                 result = limit;
-            } else if (result < 0){
+            } else if (result < 0) {
                 empty++;
                 deficit = deficit + result;
                 result = 0;
             }
-
-            // result = Math.min(state[i] + gain + loss, parameters["storage"] * 1000);
-            // // result = Math.max(result, 0)
-            // if(result < 0){
-            //     deficit = deficit + result;
-            //     empty++;
-            //     result = 0;
-            // } else if ( result + state[i] > parameters["storage"] * 1000){
-            //     surplus = surplus + result - state[i];
-            // }
-            state.push(result)
-
-            // ctx.beginPath();
-            // ctx.arc((i + 1) % 365 * scaleFactorWidth + 4, baseline - (state[i + 1] / 1000) * scaleFactorHeight, 2, 0, 2 * Math.PI);
-            // ctx.fill();
-
-            // if(result === 0){
-            //     ctx.fillStyle = "#4CE0D2";
-            // } else if (result === parameters["storage"] * 1000) {
-            //     ctx.fillStyle = "green";
-            // } else {
-            //     ctx.fillStyle = "black";
-            // }
-            ctx.fillRect((i + 1) % 365 * scaleFactorWidth, baseline - (state[i + 1] / 1000) * scaleFactorHeight, 2, 2)
+            state.push(result);
+            const x = i % 365 * scaleFactorWidth;
+            const y = baseline - (state[i + 1] / 1000) * scaleFactorHeight
+            ctx.fillRect(x, y, pointSize, pointSize);
         }
-        value_surplus.textContent = Math.round((surplus / 11) / 1000,0);
-        value_deficit.textContent = Math.round((deficit / 11) / -1000,0);
+        value_surplus.textContent = Math.round((surplus / 11) / 1000, 0);
+        value_deficit.textContent = Math.round((deficit / 11) / -1000, 0);
         value_full.textContent = Math.round((full / 11), 0);
         value_empty.textContent = Math.round((empty / 11), 0);
-        // console.timeEnd("Draw")
     }
 
-    function drawPath() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.beginPath();
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         const state = [];
-        state.push(parameters["storage"] * 1000);
+        const limit = parameters["storage"] * 1000;
+        state.push(limit);
         const scaleFactorHeight = baseline / parameters["storage"];
-        ctx.moveTo(0, baseline - parameters["storage"] * scaleFactorHeight);
 
-        let gain;
-        let loss;
-        let result;
+        let deficit = 0;
+        let empty = 0;
+        let surplus = 0;
+        let full = 0;
 
-        // console.time("Draw")
+        let position = 0;
+
         for (let i = 0; i < data["Energy"].length; i++) {
-            gain = parameters["solarArea"] * data["Energy"][i];
-            loss = Math.min(parameters["htc"] * (data["Temperature"][i] - parameters["indoorT"]) * 24, 0);
-            result = Math.min(state[i] + gain + loss, parameters["storage"] * 1000);
-            result = Math.max(result, 0)
-            state.push(result)
-            if ((i + 1) % 365 === 0) {
-                ctx.moveTo(0, baseline - (state[i + 1] / 1000) * scaleFactorHeight)
+            const gain = parameters["solarArea"] * data["Energy"][i] * parameters["efficiency"];
+            const loss = Math.min(parameters["htc"] * (data["Temperature"][i] - parameters["indoorT"]) * 24, 0);
+
+            const result = gain + loss + state[i];
+
+            if (result > limit) {
+                state.push(limit)
+                full++;
+                surplus = surplus + result - limit;
+            } else if (result < 0) {
+                state.push(0)
+                empty++;
+                deficit = deficit + result;
             } else {
-                ctx.lineTo((i + 1) % 365, baseline - (state[i + 1] / 1000) * scaleFactorHeight);
+                state.push(result)
+            }
+
+            if (i == 0) continue;
+
+            if (state[i + 1] !== state[i]) {
+                index = i % 365;
+                if (position <= index) {
+                    const x = position * scaleFactorWidth;
+                    const y = baseline - (state[i] / 1000) * scaleFactorHeight;
+                    ctx.fillRect(x, y, index - position + pointSize, pointSize);
+                } else {
+                    const x = position * scaleFactorWidth;
+                    const y = baseline - (state[i] / 1000) * scaleFactorHeight;
+                    ctx.fillRect(x, y, 365 - position, pointSize);
+                    ctx.fillRect(0, y, index - 0, pointSize);
+                }
+                position = index;
             }
         }
-        ctx.stroke();
-        // console.timeEnd("Draw")
+        value_surplus.textContent = Math.round((surplus / 11) / 1000, 0);
+        value_deficit.textContent = Math.round((deficit / 11) / -1000, 0);
+        value_full.textContent = Math.round((full / 11), 0);
+        value_empty.textContent = Math.round((empty / 11), 0);
     }
 }
